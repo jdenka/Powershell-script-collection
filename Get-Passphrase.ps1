1c2fd58f86202d6cdc927c4c30528b95b0ec9d11
Dennis <dennis@soet.info>
2023-03-22 10:16:21 +0100

    new

---
+++ Get-Passphrase.ps1
@@ -0,0 +1,22 @@
+<#
+    This passphrase generator uses the RandomLists API to generate a random passphrase.
+    It is completly written with Github Copilot and Chat GPT-3.
+#>
+function Get-Passphrase {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Delimiter
+    )
+    try {
+        $uri = "https://www.randomlists.com/data/words.json"
+        $json = Invoke-RestMethod -Uri $uri
+        $words = $json.data | Get-Random -Count 4
+        $passphrase = $words -join $Delimiter
+        Write-Output "Passphrase: $passphrase"
+    }
+    catch {
+        Write-Error "Error: $($Error[0].Exception.Message)"
+    }
+}
