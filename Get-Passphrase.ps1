fcf1c05445339a77e6543408f96e122d79a54cff
Dennis <dennis@soet.info>
2023-04-06 14:23:43 +0200

    small update

--- Get-Passphrase.ps1
+++ Get-Passphrase.ps1
@@ -5,10 +5,13 @@
 function Get-Passphrase {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false)]
         [ValidateNotNullOrEmpty()]
         [string]$Delimiter
     )
+    if (-not $Delimiter) {
+        $Delimiter = "-"
+    }
     try {
         $uri = "https://www.randomlists.com/data/words.json"
         $json = Invoke-RestMethod -Uri $uri
