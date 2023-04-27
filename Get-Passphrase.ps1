26d7c0d80115c0366aeccbec2290090fe1f55346
Dennis Johansson <dennis@soet.info>
2023-04-27 14:37:03 +0200

    Update

--- Get-Passphrase.ps1
+++ Get-Passphrase.ps1
@@ -1,6 +1,27 @@
 <#
-    This passphrase generator uses the RandomLists API to generate a random passphrase.
-    It is completly written with Github Copilot and Chat GPT-3.
+.SYNOPSIS
+This passphrase generator uses the RandomLists API to generate a random passphrase.
+It is partly written with Github Copilot and GPT-3
+
+.DESCRIPTION
+Get a random passphrase using https://www.randomlists.com/data/words.json as a source.
+It then combines 4 random words from the list with a random number between 100 and 1000.
+
+.PARAMETER Delimiter
+This parameter identifies what kind of delimiter to use between the words.
+
+.PARAMETER Count
+This parameter identifies how many passphrases to generate.
+
+.EXAMPLE
+1) Get-Passphrase -Delimiter "_" -Count 10 | Out-File -FilePath "C:\temp\passphrases.txt"
+Will generate a file with 10 passphrases separated by an underscore.
+2) Get-Passphrase 
+Will generate a single passphrase separated by a dash by default, if you are using the default values.
+The script will ask about delimiter and how many passphrases to generate.
+
+.NOTES
+Author: Dennis Johansson
 #>
 function Get-Passphrase {
     [CmdletBinding()]
@@ -46,7 +67,7 @@ function Get-Passphrase {
     if ($Count -gt 1) {
         foreach ($i in 1..$Count) {
             $words = $json.data | Get-Random -Count 4
-            $end = Get-Random -Maximum 100
+            $end = Get-Random -Maximum 1000 -Minimum 100
             $capitalized = @()
             foreach ($word in $words) {
                 $word = $word.Substring(0, 1).ToUpper() + $word.Substring(1)
@@ -59,7 +80,7 @@ function Get-Passphrase {
     else {
         $words = $json.data | Get-Random -Count 4
         $capitalized = @()
-        $end = Get-Random -Maximum 100
+        $end = Get-Random -Maximum 1000 -Minimum 100
         foreach ($word in $words) {
             $word = $word.Substring(0, 1).ToUpper() + $word.Substring(1)
             $capitalized += $word
