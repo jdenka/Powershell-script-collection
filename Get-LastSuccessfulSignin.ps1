4adc4b128ac6df108f2b4b85e1ea5ba9fef8860f
Dennis Johansson <dennis@soet.info>
2023-12-07 23:01:02 +0100

    multiuser support

--- Get-LastSuccessfulSignin.ps1
+++ Get-LastSuccessfulSignin.ps1
@@ -11,21 +11,43 @@ function Get-LastSuccessfulSignin {
         Write-Error "Please connect to Microsoft Graph.."
         break
     }
-    # User lookup
-    try {
-        $u = Get-MgUser -UserId $user -ErrorAction Stop
+    if ($user -gt 1) {
+        foreach ($mu in $user) {
+            # User lookup
+            try {
+                $u = Get-MgUser -UserId $mu -ErrorAction Stop
+            }
+            catch {
+                Write-Error "Failed to get a user with UPN/ID: $($mu)"
+            }
+            # Getting users last successfull logon
+            $uri = "https://graph.microsoft.com/beta/users/$($u.Id)?select=signInActivity"
+            try {
+                $result = Invoke-MgGraphRequest -Method GET -Uri $uri -ErrorAction Stop
+                Write-Output "Last successful sign in from user $($u.UserPrincipalName) `n $($result.signInActivity.lastSuccessfulSignInDateTime) `n Last successful non interactive sign in `n $($result.signInActivity.lastNonInteractiveSignInDateTime)"
+            }
+            catch {
+                Write-Error "Failed to get last successful sign in for user $($u.UserPrincipalName)"
+            }
+        }
     }
-    catch {
-        Write-Error "Failed to get a user with UPN/ID: $($user)"
-        break
-    }
-    # Getting users last successfull logon
-    $uri = "https://graph.microsoft.com/beta/users/$($u.Id)?select=signInActivity"
-    try {
-        $result = Invoke-MgGraphRequest -Method GET -Uri $uri -ErrorAction Stop
-        Write-Output "Last successful sign in from user $($u.UserPrincipalName) `n $($result.signInActivity.lastSuccessfulSignInDateTime) `n Last successful non interactive sign in `n $($result.signInActivity.lastNonInteractiveSignInDateTime)"
-    }
-    catch {
-        Write-Error "Failed to get last successful sign in for user $($u.UserPrincipalName)"
+    else {
+        # User lookup
+        try {
+            $u = Get-MgUser -UserId $user -ErrorAction Stop
+        }
+        catch {
+            Write-Error "Failed to get a user with UPN/ID: $($user)"
+            break
+        }
+        # Getting users last successfull logon
+        $uri = "https://graph.microsoft.com/beta/users/$($u.Id)?select=signInActivity"
+        try {
+            $result = Invoke-MgGraphRequest -Method GET -Uri $uri -ErrorAction Stop
+            Write-Output "Last successful sign in from user $($u.UserPrincipalName) `n $($result.signInActivity.lastSuccessfulSignInDateTime) `n Last successful non interactive sign in `n $($result.signInActivity.lastNonInteractiveSignInDateTime)"
+        }
+        catch {
+            Write-Error "Failed to get last successful sign in for user $($u.UserPrincipalName)"
+        }
     }
 }
